1776b3f549946608d21b4b742041f93ac1a55eea
PretellP <elw.danielpretell@gmail.com>
2023-10-25 23:38:28 -0500

    Update[Sales Form v.01]

--- src/App.js
+++ src/App.js
@@ -4,97 +4,223 @@ import React, { useState } from "react";
 
 function App() {
 
-  const [name, setName] = useState("")
-  const [numberError, setNumberError] = useState(0)
+	const [name, setName] = useState("")
+	const [numberError, setNumberError] = useState(0)
 
-  const changeName = (e) => {
-    const value = e.target.value;
-    console.log(value)
-    const minValue = value.length > 3
-    const maxValue = value.length < 10
-    const onliLet = /^[a-zA-Z\s]*$/g.test(value)
+	const changeName = (e) => {
+		const value = e.target.value;
+		console.log(value)
+		const minValue = value.length > 3
+		const maxValue = value.length < 10
+		const onliLet = /^[a-zA-Z\s]*$/g.test(value)
 
-    console.log("Min: ", minValue)
-    console.log("Max: ", maxValue)
-    console.log("onlite: ", onliLet)
+		console.log("Min: ", minValue)
+		console.log("Max: ", maxValue)
+		console.log("onlite: ", onliLet)
 
-    if (onliLet == false) {
-      setNumberError(1)
-    }
-    else if (!minValue) {
-      setNumberError(2)
-    }
-    else {
-      setNumberError(3)
-    }
+		if (onliLet == false) {
+			setNumberError(1)
+		}
+		else if (!minValue) {
+			setNumberError(2)
+		}
+		else {
+			setNumberError(3)
+		}
 
-    if (onliLet == true && minValue && maxValue) {
-      setNumberError(0)
-    }
-    setName(value)
-  }
+		if (onliLet == true && minValue && maxValue) {
+			setNumberError(0)
+		}
+		setName(value)
+	}
 
-  const saveClick = () => {
-    console.log("Este es mi estado local: ", name)
-  }
+	const saveClick = () => {
+		console.log("Este es mi estado local: ", name)
+	}
 
-  return (
+	return (
 
-    <div className="mi_formulario__container">
-      <div className="mi_formulario__header">
-        <h1 className="mi_formulario__title">Mi formulario</h1>
-      </div>
+		<div className="mi_formulario__container">
+			<div className="mi_formulario__header">
+				<h1 className="mi_formulario__title">Formulario de Ventas</h1>
+			</div>
 
-      <div className="mi_formulario__body">
-        <div className="mi_formulario__form">
-          <label className="mi_formulario__label">Nombre: </label>
-          <div className="mi_formulario__input-icon">
-            <i className="fa fa-user mi_formulario__icon" aria-hidden="true" />
-            <input
-              id="nombre"
-              name="nombre"
-              value={name}
-              onChange={changeName}
-              className="mi_formulario__input"
-            ></input>
+			<b>
+				<u>
+					Ventas
+				</u>
+			</b>
+			<br />
 
-            {numberError == 1 && (
-              <label className="mi_formulario__error">
-                El nombre solo se puede incluir letras
-              </label>
-            )}
+			<hr />
 
-            {numberError == 2 && (
-              <label className="mi_formulario__error">
-                El nombre minimo es 3 caracteres
-              </label>
-            )}
+			<div className="mi_formulario__body">
 
-            {numberError == 3 && (
-              <label className="mi_formulario__error">
-                El nombre maximo es 10 caracteres
-              </label>
-            )}
+				<div className="mi_formulario__form">
 
-          </div>
+					<div>
+						<label className="mi_formulario__label">
+							Código:
+						</label>
+						<div className="mi_formulario__input-icon">
+							<i className="fa fa-user mi_formulario__icon" aria-hidden="true" />
+							<input
+								id="code"
+								name="code"
+								value={name}
+								onChange={changeName}
+								className="mi_formulario__input"
+							></input>
+							<br />
+							{numberError == 1 && (
+								<label className="mi_formulario__error">
+									El código solo se puede incluir letras
+								</label>
+							)}
+							{numberError == 2 && (
+								<label className="mi_formulario__error">
+									El código minimo es 3 caracteres
+								</label>
+							)}
+							{numberError == 3 && (
+								<label className="mi_formulario__error">
+									El código maximo es 10 caracteres
+								</label>
+							)}
+						</div>
+					</div>
 
-          <button disabled={numberError > 0} onClick={saveClick} className="mi_formulario__button">
-            Guardar
-          </button>
+					<div>
+						<label htmlFor="invoyce_type_select">
+							Comprobante:
+						</label>
+						<div>
+							<select name="invoice_type" id="invoyce_type_select">
+								<option value="1">FACTURA</option>
+								<option value="2">BOLETA</option>
+							</select>
+						</div>
+					</div>
 
-        </div>
+					<div>
+						<label htmlFor="input_client">Cliente:</label>
+						<div>
+							<input type="text" name='client' id='input_client' />
+						</div>
+					</div>
 
-      </div>
+					<div>
+						<label htmlFor="input_date">
+							Fecha:
+						</label>
+						<div>
+							<input type="date" />
+						</div>
+					</div>
 
-      <div className="mi_formulario__footer">
-        <div className="mi_formulario__group">
-          <span className="mi_formulario__label">
-            Este es mi primer formulario con CSS
-          </span>
-        </div>
-      </div>
-    </div>
-  )
+					<div>
+						<label htmlFor="input_igv">
+							IGV:
+						</label>
+						<div>
+							<input type="number" step="0.1" />
+						</div>
+					</div>
+
+				</div>
+
+				<hr />
+
+				<div>
+
+					<div>
+						<label htmlFor="article_select">
+							Artículo:
+						</label>
+						<div>
+							<select name="article" id="article_select">
+								<option value="014">SOPA DE PASTA</option>
+								<option value="977">AVENA</option>
+								<option value="056">FRIJOL</option>
+								<option value="076">ARROZ</option>
+								<option value="065">GELATINA</option>
+								<option value="051">ATÚN</option>
+								<option value="057">SHAMPOO</option>
+							</select>
+						</div>
+					</div>
+
+					<div>
+						<label htmlFor="">
+							Cantidad:
+						</label>
+						<div>
+							<input type="number" step="1" name='quantity' min="1" />
+						</div>
+					</div>
+
+					<div>
+						<label htmlFor="input_buy_price">
+							Precio de compra
+						</label>
+						<div>
+							<input type="number" id="input_buy_price" name="buy_price" step="0.1" min="0" />
+						</div>
+					</div>
+
+					<div>
+						<label htmlFor="">
+							Precio de venta
+						</label>
+						<div>
+							<input type="number" name='sell_price' step="0.1" min="0" />
+						</div>
+					</div>
+
+					<div>
+						<label htmlFor="input_discount">
+							Descuento
+						</label>
+						<div>
+							<input type="number" name='discount' id='input_discount' step="0.1" min="0" />
+						</div>
+					</div>
+
+					<div>
+						<label htmlFor="input_expiration_date">
+							Fecha de vencimiento:
+						</label>
+						<div>
+							<input type="date" name="expiration_date" id="input_expiration_date" />
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+			<hr />
+
+			<div>
+
+			</div>
+
+			<br />
+
+			<button disabled={numberError > 0} onClick={saveClick} className="mi_formulario__button">
+				Guardar
+			</button>
+
+
+			<div className="mi_formulario__footer">
+				<div className="mi_formulario__group">
+					<span className="mi_formulario__label">
+						{/* Este es mi primer formulario con CSS */}
+					</span>
+				</div>
+			</div>
+		</div>
+	)
 
 }
 
